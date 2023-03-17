4ad1ad07a2174b226e2d3297edc9f0c962b8a005
Adithya Krishnan <adiadithyakrishnan@gmail.com>
2023-03-17 11:00:16 +0530

    feat member's list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import FAQ from "@/components/FAQ";
 import Buttons from "@/components/Buttons";
-import BarChart from "@/components/Charts";
 
 export default function Home() {
   return (
